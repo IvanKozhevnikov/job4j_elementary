80d329a14c459e0caaef8c0a9746d8930ac20c4b
Ivan Kozhevnikov <xx71@mail.ru>
2020-10-30 22:30:56 +0300

    3. Арифметические операции. [#395991]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    int a = 18;
+    float b = 78.0332F;
+    boolean c = true;
+    int age = 18;
+
+    char sex = 'm';
+    short height = 160;
+    int bricks = 3000000;
+    long bacteria = 47800000000000L;
+    long distanceToStars = 20000000000000L;
+    boolean workPermit = true;
+    int fileSize = 200000000;
+}
