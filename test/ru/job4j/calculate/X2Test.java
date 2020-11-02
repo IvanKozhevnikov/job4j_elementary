46ba1657d99f19ef3b4a8ec6dba5e626ddfaa356
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-02 14:43:59 +0300

    1.1. Входные данные в тесте [#396016]

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -6,22 +6,46 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-    public void whenA10B9C0X2Then40() {
-        int a = 1; int b = 1; int c = 1; int x = 1;
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = a * x * x + b * x + c;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-        a = 0; b = 1; c = 1; x = 1;
-        expected = a * x * x + b * x + c;
-        rsl = X2.calc(a, b, c, x);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = a * x * x + b * x + c;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-        a = 1; b = 1; c = 0; x = 1;
-        expected = a * x * x + b * x + c;
-        rsl = X2.calc(a, b, c, x);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = a * x * x + b * x + c;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-        a = 1; b = 1; c = 1; x = 0;
-        expected = a * x * x + b * x + c;
-        rsl = X2.calc(a, b, c, x);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = a * x * x + b * x + c;
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 }
