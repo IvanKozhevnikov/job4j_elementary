ed7153ed0bd372b19acc26e5d2b1ad1a69c2816b
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-03 18:01:24 +0300

    4.1.2. Глупый бот [#395921]

--- src/ru/job4j/condition/ru/job4j/condition/StringEq.java
+++ src/ru/job4j/condition/ru/job4j/condition/StringEq.java
@@ -3,7 +3,13 @@ package ru.job4j.condition.ru.job4j.condition;
 public class StringEq {
     public static void main(String[] args) {
         String first = new String("one");
+        System.out.println(first.hashCode()); //=110182
+        System.out.println(first); //=one
+
         String one = new String("one");
+        System.out.println(one.hashCode()); //=110182
+        System.out.println(new String("one")); //=one
+
         if (first == one) {
             System.out.println("first equals one");
         } else {
