549d862112218ba36d938469515fe43d9c551a04
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-04 17:49:43 +0300

    4.3.6. Math.abs [#395937]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -5,7 +5,6 @@ public class ChessBoard {
         int rsl = 0;
         if (x1 != x2 && y1 != y2 && Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
             rsl = Math.abs(x2 - x1);
-            //rsl = x1 == x2 && y1 == y2 ? rsl : rsl;
         }
         return rsl;
     }
