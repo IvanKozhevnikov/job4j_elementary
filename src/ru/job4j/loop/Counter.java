3945cf4e40a758c96eb3550212739d34a9c075bf
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-05 15:47:05 +0300

    5.0.1. Сумма чисел. [#396040]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -13,5 +13,7 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println(sum(1000, 1050));
+        System.out.println(sum(70, 89));
     }
 }
