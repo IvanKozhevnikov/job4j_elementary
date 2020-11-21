8d31b86dd8970991a70a14bc756a35a7831fde1f
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-21 22:52:40 +0300

    1. Экзамен. Базовый синтаксис. [#396022]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,13 +3,22 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int count = 0;
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-            count++;
-        }
-        for (int j = 0; j < right.length; j++) {
-            rsl[count++] = right[j];
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            if (count1 < left.length) {
+                if (count2 < right.length) {
+                    if (left[count1] < right[count2]) {
+                        rsl[i] = left[count1++];
+                    } else {
+                        rsl[i] = right[count2++];
+                    }
+                } else {
+                    rsl[i] = left[count1++];
+                }
+            } else {
+                rsl[i] = right[count2++];
+            }
         }
         return rsl;
     }
