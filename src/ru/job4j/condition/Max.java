322a776aacf3a96d04e86fe8cda5f47110591e87
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-22 20:38:30 +0300

    3. Перегрузить метод max для трех чисел [#396137]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,8 +1,16 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = right > left ? right : left;
-        return result;
+
+    public static int max(int first, int second) {
+        return second > first ? second : first;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(max(first, second, third), fourth);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 
     @Test
-    public void whenMaxmin10Tomin7Thenmin7() {
+    public void whenMaxMin10ToMin7ThenMin7() {
         int result = Max.max(-10, -7);
         assertThat(result, is(-7));
     }
@@ -22,5 +22,17 @@ public class MaxTest {
     public void whenMax330To257Then330() {
         int result = Max.max(330, 257);
         assertThat(result, is(330));
+    }
+
+    @Test
+    public void whenMax330To257To400To578Then330() {
+        int result = Max.max(330, 257, 400, 578);
+        assertThat(result, is(578));
+    }
+
+    @Test
+    public void whenMax330ToMin15To1900Then330() {
+        int result = Max.max(330, -15, 1900);
+        assertThat(result, is(1900));
     }
 }
