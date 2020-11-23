47bf077e722f922b7dc571b9aa30628e78493d8e
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-23 20:41:46 +0300

    2. Перегрузка конструктора [#396150]

---
+++ src/ru/job4j/tracker/Item.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+    }
+
+    public Item(int id, String name) {
+    }
+}
