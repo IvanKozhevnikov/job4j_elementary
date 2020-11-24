0e15895fcc29a6802318175834c975cab88ec1c3
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-24 14:28:26 +0300

    2. Перегрузка конструктора [#396150]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -8,8 +8,11 @@ public class Item {
     }
 
     public Item(int id) {
+        this.id = id;
     }
 
     public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 }
