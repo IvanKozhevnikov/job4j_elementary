7e64e90c6ffebab3bcb1ebe08a5982028d506755
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-25 00:16:04 +0300

    2. Модель данных. [#396158]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Кожевников Иван Михайлович");
+        student.setGroup("Стажёр");
+        student.setDateOfReceipt(new Date(2020, Calendar.NOVEMBER, 25, 11, 45, 15));
+        System.out.println(student.getFullName() + "\n"
+                + student.getGroup() + "\n" +  student.getDateOfReceipt());
+
+    }
+}
