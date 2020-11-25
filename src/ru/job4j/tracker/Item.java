dc32f892fece72658cade50f47cafac521d1787e
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-25 15:01:52 +0300

    7. Date. Отображение даты. [#396154]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -7,10 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item() {
     }
 
@@ -21,5 +17,9 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
