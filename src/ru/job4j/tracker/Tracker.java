80fd13e22e8774215da06eab227931480f12937a
Ivan Kozhevnikov <xx71@mail.ru>
2020-11-27 17:04:06 +0300

    6. Метод замены заявки. Tracker.replace [#396162]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -29,21 +29,34 @@ public class Tracker {
         for (int i = 0; i < size; i++) {
             Item item = items[i];
             if (key.equals(item.getName())) {
-               rsl[i] = items[i];
+                rsl[i] = items[i];
+            }
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                rsl = i;
+                break;
             }
         }
         return rsl;
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
+        int i = indexOf(id);
+        return i != -1 ? items[i] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int i = indexOf(id);
+        if (i != -1) {
+            item.setId(id);
+            items[i] = item;
         }
-        return rsl;
+        return false;
     }
 }
