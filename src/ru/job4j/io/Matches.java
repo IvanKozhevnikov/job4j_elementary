6f996627a60ae89c897bf103c06d90805bc12e7a
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-01 20:17:48 +0300

    2. Scanner и чтение числа из консоли. [#396088]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,32 +6,30 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int count = 11;
+        int selectFirst;
+        int selectSecond;
         while (count > 0) {
             do {
                 System.out.println("Игрок 1: берите спички.");
-                int selectFirst = Integer.valueOf(input.nextLine());
-                count -= selectFirst;
-
-                if (count > 0) {
-                    System.out.println("Количество спичек на столе " + count);
-                    System.out.println();
-                }
-                if (count <= 0) {
-                    System.out.println("Игрок 1 выйграл");
-                    break;
-                }
+                selectFirst = Integer.parseInt(input.nextLine());
+            } while (selectFirst < 1 || selectFirst > 3);
+            count -= selectFirst;
+            if (count > 0) {
+                System.out.println("Количество спичек на столе " + count);
+            } else {
+                System.out.println("Игрок 1 выйграл");
+                break;
+            }
+            do {
                 System.out.println("Игрок 2: берите спички.");
-                int selectSecond = Integer.valueOf(input.nextLine());
-                count -= selectSecond;
-                if (count > 0) {
-                    System.out.println("Количество спичек на столе " + count);
-                    System.out.println();
-                }
-                if (count <= 0) {
-                    System.out.println("Игрок 1 выйграл");
-                    break;
-                }
-            } while (count > 0);
+                selectSecond = Integer.parseInt(input.nextLine());
+            } while (selectSecond < 1 || selectSecond > 3);
+            count -= selectSecond;
+            if (count > 0) {
+                System.out.println("Количество спичек на столе " + count);
+            } else {
+                System.out.println("Игрок 2 выйграл");
+            }
         }
     }
 }
