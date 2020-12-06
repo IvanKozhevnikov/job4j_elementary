48e82bec20bb64ca6ab6362b86479efd3162f1e5
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-06 19:45:45 +0300

    0.3. Тестирование исключений с junit [#396108]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -2,12 +2,11 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-
     }
 
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Start should be less then finish.");
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
