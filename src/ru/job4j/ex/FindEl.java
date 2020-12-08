ce2c62630d6de22d92728bc870fcebd89c9f6166
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-08 14:34:05 +0300

    0.4. Пользовательские исключения. [#396109]

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -3,9 +3,12 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
+        int count = 0;
+        for (String i : value) {
+            count++;
+            if (i.equals(key)) {
+                rsl = count - 1;
+                break;
             }
         }
         if (rsl == -1) {
@@ -15,8 +18,8 @@ public class FindEl {
     }
 
     public static void main(String[] args) {
-        String[] array = {"Ivan", "Alexander"};
-        String key = "Ivan";
+        String[] array = {"Ivan", "Alexander","Vasiliy"};
+        String key = "Vasiliy";
         try {
             System.out.println(indexOf(array, key));
         } catch (ElementNotFoundException e) {
@@ -24,3 +27,14 @@ public class FindEl {
         }
     }
 }
+  /*  int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+        if (value[i].equals(key)) {
+        rsl = i;
+        }
+        }
+        if (rsl == -1) {
+        throw new ElementNotFoundException("there is no such element in the array");
+        }
+        return rsl;
+        }*/
