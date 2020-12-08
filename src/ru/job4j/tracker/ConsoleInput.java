d792509d0432c6745d78070864d8d00a17333a76
Ivan Kozhevnikov <xx71@mail.ru>
2020-12-08 23:41:27 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#396114]

--- src/ru/job4j/tracker/ConsoleInput.java
+++ src/ru/job4j/tracker/ConsoleInput.java
@@ -3,21 +3,18 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class ConsoleInput implements Input {
-    private final Output out;
 
-    public ConsoleInput(Output out) {
-        this.out = out;
-    }
+
     private Scanner scanner = new Scanner(System.in);
 
     @Override
     public String askStr(String question) {
-        out.println(question);
+        System.out.println(question);
         return scanner.nextLine();
     }
 
     @Override
     public int askInt(String question) {
-        return Integer.valueOf(askStr(question));
+        return Integer.parseInt(askStr(question));
     }
 }
